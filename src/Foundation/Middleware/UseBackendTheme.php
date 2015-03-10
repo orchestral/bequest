f941a5f8e3bccb927d9590f1f2a9ab4ea1224c40
crynobone <crynobone@gmail.com>
2015-03-10 12:48:01 +0800

    Add Orchestra\Foundation\Middleware\UseBackendTheme

---
+++ src/Foundation/Middleware/UseBackendTheme.php
@@ -0,0 +1,8 @@
+<?php namespace Orchestra\Foundation\Middleware;
+
+use Orchestra\Foundation\Http\Middleware\UseBackendTheme as BaseMiddleware;
+
+class UseBackendTheme extends BaseMiddleware
+{
+    //
+}
