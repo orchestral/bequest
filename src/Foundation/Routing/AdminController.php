7d52c9ec874e8232633e33720650c34a77ee00f0
crynobone <crynobone@gmail.com>
2015-03-10 12:37:40 +0800

    First.
    Signed-off-by: crynobone <crynobone@gmail.com>

---
+++ src/Foundation/Routing/AdminController.php
@@ -0,0 +1,8 @@
+<?php namespace Orchestra\Foundation\Routing;
+
+use Orchestra\Foundation\Http\Controllers\AdminController as Controller;
+
+abstract class AdminController extends Controller
+{
+    //
+}
---
+++ src/Foundation/Routing/BaseController.php
@@ -0,0 +1,8 @@
+<?php namespace Orchestra\Foundation\Routing;
+
+use Orchestra\Foundation\Http\Controllers\BaseController as Controller;
+
+abstract class BaseController extends Controller
+{
+    //
+}
